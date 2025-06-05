47dabcc7f3f0a80a70dc7de6933972dba847e790
FabianTG25 <fab.rey.2701@gmail.com>
2025-06-04 21:58:43 -0500

    first commit: base project

---
+++ src/main/java/upc/edu/pe/viatmeet_backend/ViatmeetBackendApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.viatmeet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViatmeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViatmeetBackendApplication.class, args);
+	}
+
+}
