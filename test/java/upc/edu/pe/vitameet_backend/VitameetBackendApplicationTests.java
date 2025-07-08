7162f1c3c39f67810530cf285dc5ff7ce1d8cd49
Gossk <vidalmiguel535@gmail.com>
2025-07-08 18:11:26 -0500

    fix: backend

---
+++ test/java/upc/edu/pe/vitameet_backend/VitameetBackendApplicationTests.java
@@ -0,0 +1,11 @@
+
+package upc.edu.pe.vitameet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitameetBackendApplicationTests {
+    @Test
+    void contextLoads() {}
+}
